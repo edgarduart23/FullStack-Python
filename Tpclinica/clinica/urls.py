30872be43b7112cf9029fd1c1bbeddc0bc9e8204
edgardo <nergumero2017@gmail.com>
2020-11-19 22:49:59 -0300

    productos

--- Tpclinica/clinica/urls.py
+++ Tpclinica/clinica/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name= "clinica"
 urlpatterns = [
     path('',  views.index, name="index"),
-    
+    path('productos',  views.productos, name="productos"),
+    path('productos/<int:producto_id>',  views.producto, name="producto"),
+    path('productos/agregar',  views.agregar, name="agregar"),
+    path('productos/eliminar/<int:producto_id>',  views.eliminar, name="eliminar"),
+    path('productos/actualizar/<int:producto_id>',  views.actualizar, name="actualizar"),
 ]
--- Tpclinica/clinica/views.py
+++ Tpclinica/clinica/views.py
@@ -1,4 +1,58 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Producto
+from .form import ProductoCreate
+from django.http import HttpResponse
 # Create your views here.
-<h1>comienzo</h1>
+def index(request):
+    return render(request, "index.html")
+
+
+def productos(request):
+    return render(request, "productos.html", {
+        "productos": Producto.objects.all()
+    })
+
+def producto(request, producto_id):
+        unProducto = Producto.objects.get(id=producto_id)
+        return render(request, "producto.html",{
+            "producto": unProducto
+        })
+
+def agregar(request):
+    upload  = ProductoCreate()
+    if request.method == 'POST':
+        upload = ProductoCreate(request.POST, request.FILES)
+        if upload.is_valid():
+            upload.save()
+            return redirect('clinica:productos')
+        else:
+            return HttpResponse("""your form is wrong, reload on <a href = "{{ url : 'clinica:productos'}}">reload</a>""")
+    else:
+        return render(request, 'agregar.html', {'upload_form':upload})
+
+
+
+def eliminar(request, producto_id):
+    producto_id= int(producto_id)
+    try:
+        producto_sel = Producto.objects.get(id = producto_id)
+        
+    except Producto.DoesNotExist:
+        return redirect('clinica:productos')
+    producto_sel.delete()
+    return render(request, "eliminar.html")
+
+
+
+def actualizar(request, producto_id):
+     producto_id = int(producto_id)
+     try:
+         producto_sel = Producto.objects.get(id = producto_id)
+     except Producto.DoesNotExist:
+         return redirect('index')
+     producto_form = ProductoCreate(request.POST or None, instance = producto_sel)
+     if producto_form.is_valid():
+        producto_form.save()
+        return redirect('clinica:productos')
+     return render(request, 'agregar.html', {'upload_form':producto_form })
+        
