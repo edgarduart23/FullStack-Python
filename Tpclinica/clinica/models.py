06edcfd7a87d8ecb6e4a7e95931348b2036a9947
melisacruzmartinez <melisa.cruzmartinez@gmail.com>
2020-11-21 12:15:47 -0300

    paciente,doctor models

--- Tpclinica/clinica/models.py
+++ Tpclinica/clinica/models.py
@@ -10,10 +10,25 @@ class Producto(models.Model):
     def __str__(self):
         return f"{self.id} {self.descripcion} {self.precio} {self.tipo} "
 
+class Paciente(models.Model):
+    nombre = models.CharField(max_length = 120)
+    place = models.OneToOneField(
+        Place,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+class Doctor(models.Model):
+    place = models.OneToOneField(
+        Place,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        nombre = models.CharField(max_length = 120)
+    )
+
 class Turnos(models.Model):
     id = models.IntegerField()
-    Paciente = models.ForeignKey(paciente)
-    Doctor =models.ForeignKey(doctor)
+    Paciente = models.ForeignKey(Paciente,  on_delete=models.CASCADE)
+    Doctor =models.ForeignKey(Doctor,  on_delete=models.CASCADE)
     FechaTurno = models.DateField()
     HoraTurno = models.DateTimeField()
     FechaAlta = models.DateField()
