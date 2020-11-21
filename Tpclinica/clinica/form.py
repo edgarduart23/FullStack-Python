1514fc9abf9834462764ec2054ea2f6eba6a72ff
melisacruzmartinez <melisa.cruzmartinez@gmail.com>
2020-11-21 12:00:09 -0300

    formturno

--- Tpclinica/clinica/form.py
+++ Tpclinica/clinica/form.py
@@ -1,8 +1,13 @@
 from django import forms
 from .models import  Producto
+from.models import Turnos
 
 class ProductoCreate(forms.ModelForm):
     class Meta:
         model = Producto
         fields = '__all__'
         
+class TurnosCreate(forms.ModelForm):
+    class Meta:
+        model = Turnos
+        field = '__all__'
