d395b8b24f2cc94b508e68ddc3e1e34838ba9563
melisacruzmartinez <melisa.cruzmartinez@gmail.com>
2020-11-25 19:51:01 -0300

    añado urls turno,crear,actualizar,borrar

--- Tpclinica/clinica/urls.py
+++ Tpclinica/clinica/urls.py
@@ -15,9 +15,7 @@ urlpatterns = [
     path('historial/eliminar_consulta/<int:consulta_id>',  views.eliminar_consulta, name="eliminar_consulta"),
     path('historial/modificar_consulta/<int:consulta_id>',  views.modificar_consulta, name="modificar_consulta"),
 
-    # path('turnos/crear', views.crearTurno , name = "crearTurno"),
-    # path('turnos/actualizar/<int:turno_id>', views.actualizarTurno, name = "actualizarTurno"),
-    # path('turnos/eliminar/<int:turno_id', views.borrarTurno, name ="borrarTurno"),
+    
     path('pedidos',  views.pedidos, name="pedidos"),
     path('pedidos/<int:pedido_id>',  views.pedido, name="pedido"),
     path('pedidos/agregar',  views.agregar_pedido, name="agregar_pedido"),
@@ -26,4 +24,11 @@ urlpatterns = [
     path('pedidos/pedido_items/<int:pedido_id>/',  views.pedido_items, name="pedido_items"),
     path('pedidos/pedido_items/<int:pedido_id>/agregar_item',  views.agregar_item, name="agregar_item"),
     
+    ################################################################### Turnos
+    path ('turnos',views.turnos, name="turnos"),
+    path('turnos/crear', views.crearTurno , name = "crearTurno"),
+    path('turnos/actualizar/<int:turno_id>', views.actualizarTurno, name = "actualizarTurno"),
+    path('turnos/eliminar/<int:turno_id', views.borrarTurno, name ="borrarTurno"),
+
+    ##################################################################
 ]
