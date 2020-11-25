9b1f2791fade16ac53b3f3f832c1275f31c20056
maiano <aguirremariano2000@gmail.com>
2020-11-25 17:15:54 -0300

    no anda responseRedirect

--- Tpclinica/clinica/models.py
+++ Tpclinica/clinica/models.py
@@ -67,7 +67,7 @@ class Pedido(models.Model):
     ESTADO = (('PT', 'Pendiente'),('PD', 'Pedido'),('TL', 'Taller'),('FP', 'Finalizado'))
     estado = models.CharField( max_length=2,default='PD',choices=ESTADO)
     subtotal = models.DecimalField(max_digits=10,decimal_places=2,default=0.0,blank=True, null=True)
-    fecha = models.DateField( default= datetime.date.today)
+    fecha = models.DateField( default= datetime.datetime.today)
     
     def verSubTotal(self):
         return f"$ {self.subtotal}"
