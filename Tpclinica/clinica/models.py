b847fc69cf3edb30d9e001d029072ef9720183f4
maiano <aguirremariano2000@gmail.com>
2020-11-26 15:06:24 -0300

    error en models Pedido dataField

--- Tpclinica/clinica/models.py
+++ Tpclinica/clinica/models.py
@@ -58,7 +58,7 @@ class Pedido(models.Model):
     ESTADO = (('PT', 'Pendiente'),('PD', 'Pedido'),('TL', 'Taller'),('FP', 'Finalizado'))
     estado = models.CharField( max_length=2,default='PD',choices=ESTADO)
     subtotal = models.DecimalField(max_digits=10,decimal_places=2,default=0.0,blank=True, null=True)
-    fecha = models.DateTimeField( default= timezone.now())
+    fecha = models.DateField( default= timezone.now())
     
     def verSubTotal(self):
         return f"$ {self.subtotal}"
